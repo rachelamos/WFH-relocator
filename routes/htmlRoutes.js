46dbc4ad077eb5b7e4440e79b0a67961747cfa04
dboren <danielboren@att.net>
2021-06-01 21:12:13 -0500

    added server.js code and cleaned up route directory structure

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,5 +1,5 @@
 const router = require('express').Router();
 const { User, Event, Comment, Category, Type, Guest,Gift,Potluck } = require('../models');
-const withAuth = require('../utils/auth');
+// const withAuth = require('../utils/auth');
 
 module.exports = router;
