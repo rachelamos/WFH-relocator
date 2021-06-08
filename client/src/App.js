381a3cd6c64f2076286a920bf5d9a7dc8c08dc6f
bandreae <72764934+bandreae@users.noreply.github.com>
2021-06-07 21:49:36 -0500

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -2,13 +2,10 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Chart from './components/Rechart';
 import Nav from './components/Navbar';
-<<<<<<< HEAD
 import Login from './components/Auth/Loginpage'
 import Signup from './components/Auth/Signup'
-=======
 import Login from './pages/Login';
 import SearchPage from './pages/SearchPage';
->>>>>>> 4051e13f30908d6c141ed5d0f5e486259b0e0bfc
 
 function App () {
   return (
