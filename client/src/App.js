ef4a53a527f5235c45e54efb920ef023423562e9
andrbj <brandonjandreae@gmail.com>
2021-06-07 21:43:51 -0500

    login and signup files. authorization config json

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import Chart from './components/Rechart/Rechart';
 import Nav from './components/Navbar';
-import Login from './components/Auth/login'
+import Login from './components/Auth/Loginpage'
 import Signup from './components/Auth/Signup'
 
 function App () {
