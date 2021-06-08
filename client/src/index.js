5a02a2f12cf6a1d79cfea22d4e1e632d8ca51218
bandreae <72764934+bandreae@users.noreply.github.com>
2021-06-07 22:11:29 -0500

    Update index.js

--- client/src/index.js
+++ client/src/index.js
@@ -17,12 +17,9 @@
 // // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 // reportWebVitals();
 
-import { render } from "react-dom";
-import 'bootstrap/dist/css/bootstrap.min.css';
+import React from "react";
 import ReactDOM from "react-dom";
+import 'bootstrap/dist/css/bootstrap.min.css';
 import { Auth0Provider } from '@auth0/auth0-react';
-
 import App from "./App";
-
-const rootElement = document.getElementById("root");
-render(<App />, rootElement);
+ReactDOM.render(<App />, document.getElementById("root"));
