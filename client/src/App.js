89b674292a8c682f4f7efdad084ab11073f12f8b
bandreae <72764934+bandreae@users.noreply.github.com>
2021-06-07 21:22:36 -0500

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,8 @@ import Chart from './components/Rechart/Rechart';
 import Nav from './components/Navbar';
 import LoginPage from './components/Auth/loginpage'
 import Signup from './components/Auth/Signup'
+import Login from './pages/Login';
+import SearchPage from './pages/SearchPage';
 
 function App () {
   return (
