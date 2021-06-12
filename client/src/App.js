be792f7bce14b9bf64744c1af88b79608ddacaa7
Rachel Amos <rachelamos35@gmail.com>
2021-06-12 13:07:48 -0500

    change to app so searchpage is landing page

--- client/src/App.js
+++ client/src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 // import Chart from './components/Rechart';
 import Nav from './components/Navbar';
-import Login from './pages/Login';
+// import Login from './pages/Login';
 import SearchPage from './pages/SearchPage';
 
 function App () {
@@ -10,15 +10,15 @@ function App () {
     <Router>
     <div>
       <Nav />
-      <Switch>
-        <Route exact path={"/"}>
+      {/* <Switch> */}
+        {/* <Route exact path={"/"}>
           <Login />
-        </Route>
-        <Route exact path={"/location"}>
+        </Route> */}
+        <Route exact path={"/"}>
           <SearchPage />
           {/* <Chart /> */}
         </Route>
-      </Switch>
+      {/* </Switch> */}
     </div>
     </Router>
   )
